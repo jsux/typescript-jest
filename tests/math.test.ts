58094a192d9c50fbe4da0e28d4e61f3034cf7fea
upkar singh <imjsux@gmail.com>
2023-03-08 12:37:41 +0000

    use jest.spyon instead
    Something is causing jest.mock to fail in the setup.. using spyOn instead

--- tests/math.test.ts
+++ tests/math.test.ts
@@ -1,11 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { bonusScheme } from "../src/math";
+// import { is25PercentChance as mockdFn } from "../src/probablityCalc";
+import * as utilMod from "../src/probablityCalc";
 
-// import * as utilMod from "../src/probablityCalc";
-
-jest.mock("../src/probablityCalc", () => ({
-  is25PercentChance: jest.fn(() => true),
-}));
+const mockedIs25PercentChance = jest.spyOn(utilMod, "is25PercentChance");
 const cases = [
   [0, 0],
   [-1, 0],
@@ -30,7 +28,15 @@ test.each(cases)(
 );
 
 test("returns 50% of deposite when deposite > £10, ", () => {
+  mockedIs25PercentChance.mockReturnValue(true);
   expect(bonusScheme(50.4)).toBe(25.2);
   expect(bonusScheme(50.45)).toBe(25.23);
   expect(bonusScheme(50000.45)).toBe(25000.22);
+});
+
+test("returns 0 of deposite when deposite > £10, ", () => {
+  mockedIs25PercentChance.mockReturnValue(false);
+  expect(bonusScheme(50.4)).toBe(0);
+  expect(bonusScheme(50.45)).toBe(0);
+  expect(bonusScheme(50000.45)).toBe(0);
 });
